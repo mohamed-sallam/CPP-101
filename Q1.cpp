01f21171ce1726178407a0087ba0ecd09f001101
mohamed-sallam <59128458+mohamed-sallam@users.noreply.github.com>
2019-12-24 00:28:45 +0200

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int i=1,j,k,f=1,s=0;
+	/*int i=1,j,k,f=1,s=0;
 	float n;
 	cout<<"1!+3!+5!+7!+...+N\nEnter N: "; cin>>n;
 	while(n>1){
@@ -14,6 +14,14 @@ int main() {
 			s+=f; f=1;
 		}
 		cout<<"Sum= "<<s;
-	}else cout<<"Error";
+	}else cout<<"Error";*/ //This is the old algorithm designed by Mohamed Sallam
+	//The new one is designed by Mahmoud Abdulnasser
+	int f=1,s=0,i,n;
+	cout<<"1!+3!+5!+7!+...+N\nEnter N: "; cin>>n;
+	for(i=1;f<=n;i++){
+		f*=i;
+		if(i%2) s+=f;    //Or if(i%2==1) s+=f;
+	}
+	cout<<"Sum= "<<s; 
 }
 
